71f468a4ee8d2e5160a2b52f9d17502114beb415
Robert Norlander <robert.norlander@gmail.com>
2011-09-23 14:13:15 +0200

    Updated the http_messages files

--- http_message.c
+++ http_message.c
@@ -19,29 +19,105 @@ const char *http_methods[] =
 		"UNKNOWN"
 	}; 
 
+void http_request_init(http_request **req)
+{
+	*req = (http_request*)malloc(sizeof(http_request));
+
+	http_request *request = *req; 
+	request->method = 0; 
+	request->search_path = NULL; 
+
+	TAILQ_INIT(&request->metadata_head); 
+}
+
+void http_request_destroy(http_request *req)
+{
+}
+
+void http_request_print(http_request *req)
+{
+	printf("[HTTP_REQUEST] \n"); 
+	printf("method:\t\t%s\n", 
+		http_methods[req->method]);
+	printf("path:\t\t%s\n", 
+		req->search_path); 
+
+	printf("[Metadata] \n"); 
+	struct http_metadata_item *item; 
+	TAILQ_FOREACH(item, &req->metadata_head, entries) {
+		printf("%s: %s\n", item->key, item->value); 
+	}
+
+	printf("\n"); 
+}
+
+void http_parse_method(http_request *result, char *line)
+{
+	char *str_part = strtok(line, " ");
+	
+	int method, found = 0; 
+	for(method = 0; method < http_methods_len; method++)
+	{
+		if(strcmp(str_part, http_methods[method]) == 0)
+		{
+			found = 1; 
+			result->method = method;
+			break; 
+		}
+	}
+
+	if(!found)
+	{
+		return; 
+	}
+
+	// Get the search path from the request
+	// (perhaps this should only be done when we
+	// get a GET request?)
+	str_part = strtok(NULL, " "); 
+	result->search_path = strdup(str_part); 
+
+	// TODO: Retrieve the HTTP version
+}
+
+// Content-Byte: 101
+void http_parse_metadata(http_request *result, char *line)
+{
+	char *key = strdup(strtok(line, ":")); 
+
+	char *value = strdup(strtok(NULL, "\r")); 
+	
+	// remove whitespaces :)
+	char *p = value; 
+	while(*p == ' ') p++; 
+	value = p; 
+
+	http_metadata_item *item = (http_metadata_item*)malloc(sizeof(http_metadata_item)); 
+	item->key = key; 
+	item->value = value; 
+
+	TAILQ_INSERT_TAIL(&result->metadata_head, item, entries); 
+}
+
 
 
 char *http_build_request(http_request *req)
 {
-	const char *method = http_methods[req->method]; 
 	const char *search_path = req->search_path; 
 
-	int len = strlen(method) + 1 + strlen(search_path) +
-		1 + strlen("HTTP/1.1") + strlen("\r\n") + 1; 
-	char *request_buffer = malloc(sizeof(char)*len); 
-	memset(request_buffer, sizeof(char)*len, '\0');
+	// construct the http request 
+	int size = strlen("GET ") + 1; 
+	char *request_buffer = malloc(sizeof(char)*size);
+	memset(request_buffer, '\0', sizeof(char)*size);
+	strncat(request_buffer, "GET ", 4);
 
-	snprintf(request_buffer, len, "%s %s HTTP/1.1\r\n", 
-		method, 
-		search_path); 
+	size += strlen(search_path) + 1; 
+	request_buffer = realloc(request_buffer, size);
+	strncat(request_buffer, search_path, strlen(search_path));
 
-	len += strlen("Host: localhost\r\n\r\n"); 
-	request_buffer = realloc(request_buffer, sizeof(char)*len); 
-
-	strncat(request_buffer, "Host: localhost\r\n\r\n", len); 
-
-	printf("Created HTTP request: \n%s\n", 
-		request_buffer); 
+	size += strlen(" HTTP/1.0\r\n\r\n");
+	request_buffer = realloc(request_buffer, size); 
+	strncat(request_buffer, " HTTP/1.0\r\n\r\n", strlen(" HTTP/1.0\r\n\r\n"));
 
 	return request_buffer; 
 }
--- http_message.h
+++ http_message.h
@@ -2,6 +2,11 @@
 #define HTTP_MESSAGE_HH
 #include "proxy.h"
 
+void http_request_init(http_request**);
+void http_request_destroy(http_request*);
+void http_request_print(http_request*);
+void http_parse_method(http_request*, char*);
+void http_parse_metadata(http_request*, char*);
 char *http_build_request(http_request*); 
 
 #endif
