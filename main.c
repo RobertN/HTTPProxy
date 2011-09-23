93b9d1540c8aa6c9a54274e6e2fc3799673e0d98
Robert Norlander <robert.norlander@gmail.com>
2011-09-23 14:12:41 +0200

    Moved some functions to other files

--- main.c
+++ main.c
@@ -15,91 +15,9 @@
 #include "proxy.h"
 #include "net.h"
 #include "list.h"
+#include "http_message.h"
 
 #define PORT "8080"
-
-
-
-void http_request_init(http_request **req)
-{
-	*req = (http_request*)malloc(sizeof(http_request));
-
-	http_request *request = *req; 
-	request->method = 0; 
-	request->search_path = NULL; 
-
-	TAILQ_INIT(&request->metadata_head); 
-}
-
-void http_request_destroy(http_request *req)
-{
-}
-
-void http_request_print(http_request *req)
-{
-	printf("[HTTP_REQUEST] \n"); 
-	printf("method:\t\t%s\n", 
-		http_methods[req->method]);
-	printf("path:\t\t%s\n", 
-		req->search_path); 
-
-	printf("[Metadata] \n"); 
-	struct http_metadata_item *item; 
-	TAILQ_FOREACH(item, &req->metadata_head, entries) {
-		printf("%s: %s\n", item->key, item->value); 
-	}
-
-	printf("\n"); 
-}
-
-
-void http_parse_method(http_request *result, char *line)
-{
-	char *str_part = strtok(line, " ");
-	
-	int method, found = 0; 
-	for(method = 0; method < http_methods_len; method++)
-	{
-		if(strcmp(str_part, http_methods[method]) == 0)
-		{
-			found = 1; 
-			result->method = method;
-			break; 
-		}
-	}
-
-	if(!found)
-	{
-		return; 
-	}
-
-	// Get the search path from the request
-	// (perhaps this should only be done when we
-	// get a GET request?)
-	str_part = strtok(NULL, " "); 
-	result->search_path = strdup(str_part); 
-
-	// TODO: Retrieve the HTTP version
-}
-
-// Content-Byte: 101
-void http_parse_metadata(http_request *result, char *line)
-{
-	char *key = strdup(strtok(line, ":")); 
-
-	char *value = strdup(strtok(NULL, "\r")); 
-	
-	// remove whitespaces :)
-	char *p = value; 
-	while(*p == ' ') p++; 
-	value = p; 
-
-	http_metadata_item *item = (http_metadata_item*)malloc(sizeof(http_metadata_item)); 
-	item->key = key; 
-	item->value = value; 
-
-	TAILQ_INSERT_TAIL(&result->metadata_head, item, entries); 
-}
 
 // TODO: Test all the edge cases
 char *read_line(int sockfd)
@@ -167,28 +85,16 @@ int send_to_client(int client_sockfd, char data[], int packages_size)
             }
         }
     }
-	printf("The data is sent to the client\n");
+
     return 0;
 }
 
 int http_request_send(int sockfd, http_request *req)
 {
-	const char *search_path = req->search_path; 
-	//char request_buffer[] = "GET /index.html HTTP/1.1\r\nHost: 85.8.2.230\r\n\r\n"; 
+	LOG(LOG_TRACE, "Requesting ");
+	printf("%s\n", req->search_path);
 
-	// construct the http request 
-	int size = strlen("GET ") + 1; 
-	char *request_buffer = malloc(sizeof(char)*size);
-	memset(request_buffer, '\0', sizeof(char)*size);
-	strncat(request_buffer, "GET ", 4);
-
-	size += strlen(search_path) + 1; 
-	request_buffer = realloc(request_buffer, size);
-	strncat(request_buffer, search_path, strlen(search_path));
-
-	size += strlen(" HTTP/1.0\r\n\r\n");
-	request_buffer = realloc(request_buffer, size); 
-	strncat(request_buffer, " HTTP/1.0\r\n\r\n", strlen(" HTTP/1.0\r\n\r\n"));
+	char *request_buffer = http_build_request(req); 
 
 	// send the http request to the web server 
 	if(send(sockfd, request_buffer, strlen(request_buffer), 0) == -1)
@@ -207,43 +113,26 @@ void handle_client(int client_sockfd)
 	char *line; 
 	int server_sockfd; 
 	http_request *req; 
-	http_request_init(&req); 
 
-	line = read_line(client_sockfd); 
-	http_parse_method(req, line); 
-
-	while(1)
+	req = http_read_header(client_sockfd);
+	if(req == NULL)
 	{
-		line = read_line(client_sockfd); 
-		if(line[0] == '\r' && line[1] == '\n') 
-		{
-			// We received the end of the HTTP header 
-			break; 
-		}
-
-		// TODO: Save the headers sent by the client in
-		// a linked list or something.
-		http_parse_metadata(req, line); 
-
-		free(line); 
+		LOG(LOG_ERROR, "Failed to parse the header\n");
+		return;
 	}
 
-
-	
 	server_sockfd = http_connect(req);
 	if(server_sockfd == -1) 
 	{
-		printf("Failed to connect to host\n");
+		LOG(LOG_ERROR, "Failed to connect to host\n");
 		return; 
 	}
 
-	//printf("connected to host\n");
 	LOG(LOG_TRACE, "Connected to host\n");
 
-	// TODO: Send the request to the server 
 	http_request_send(server_sockfd, req); 
-	http_request_print(req);
 
+	LOG(LOG_TRACE, "Beginning to retrieve the response header\n");
 	while(1)
 	{
 		line = read_line(server_sockfd); 
@@ -251,14 +140,16 @@ void handle_client(int client_sockfd)
 		if(line[0] == '\r' && line[1] == '\n') 
 		{
 			// We received the end of the HTTP header 
-			break; 
+			LOG(LOG_TRACE, "Received the end of the HTTP response header\n");
 		}
 
 		free(line); 
 	}
 
+	LOG(LOG_TRACE, "Beginning to retrieve content\n");
 	char *temp = http_read_chunk(server_sockfd);
-	printf("\n\n%s\n", temp);
+	LOG(LOG_TRACE, "Received the content\n");
+
 	send_to_client(client_sockfd, temp, 1337);
 	close(server_sockfd);
 }
@@ -279,7 +170,7 @@ void start_server(unsigned int port)
 	hints.ai_socktype = SOCK_STREAM; 
 	hints.ai_flags = AI_PASSIVE; 
 
-    if((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+	if((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
 	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return; 
