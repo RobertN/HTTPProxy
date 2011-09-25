a53323b09c56926f25207cc84f3d02e793582dc5
Robert Norlander <robert.norlander@gmail.com>
2011-09-25 18:11:36 +0200

    Added list_add_key to list.c

--- list.c
+++ list.c
@@ -1,5 +1,6 @@
 #include <sys/queue.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "list.h"
 #include "proxy.h"
@@ -15,4 +16,13 @@ const char *list_get_key(struct METADATA_HEAD *list, const char *key)
 	}
 
 	return NULL;
+}
+
+void list_add_key(struct METADATA_HEAD *list, const char *key, const char *value)
+{
+	http_metadata_item *item = (http_metadata_item*)malloc(sizeof(http_metadata_item));
+	item->key = key; 
+	item->value = value; 
+
+	TAILQ_INSERT_TAIL(list, item, entries); 
 }
